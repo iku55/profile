96150ffe4a7a06956be4c01bfa6ba330a7779c9e
iku55 <ikscratch27@gmail.com>
2026-07-04 23:25:15 +0900

    Update site and base configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://iku55.github.io/',
+  base: '/profile',
   vite: {
     plugins: [tailwindcss()]
   }
